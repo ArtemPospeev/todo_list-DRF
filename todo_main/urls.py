6bb0dd947e5794153cdca8e8915ef541a0a6d4a7
Artem P <pospeev.artem@icloud.com>
2023-01-11 23:56:58 +0400

    added camel case render/ parser in settings

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -3,11 +3,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from userapp.views import CustomUserModelViewSet
+from todoapp.views import ProjectModelViewSet, ToDoModelViewSet
 
 router = DefaultRouter()
 router.register('users', CustomUserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('TODO', ToDoModelViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include(router.urls))
+    path('', include(router.urls)),
+    path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- todoapp/models.py
+++ todoapp/models.py
@@ -8,6 +8,9 @@ class Project(CustomBaseModel):
     repo_link = models.CharField(max_length=128, verbose_name='Link on repo')
     users = models.ManyToManyField(CustomUser, verbose_name='Users')
 
+    def __str__(self):
+        return f'number: {self.number} | name: {self.name} '
+
 
 class ToDo(CustomBaseModel):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Project')
@@ -15,3 +18,6 @@ class ToDo(CustomBaseModel):
     number = models.IntegerField(editable=False, unique=True, verbose_name='ToDo number')
     body = models.TextField(max_length=500, verbose_name='ToDo body')
     is_active = models.BooleanField(default=True, verbose_name='Is active')
+
+    def __str__(self):
+        return f'Project: {self.project} | Task # {self.creator}'
