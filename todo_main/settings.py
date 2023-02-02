be86e8fefed7f0e718824147e1df3e6d514022e6
Artem <pospeev.artem@icloud.com>
2023-02-03 03:24:26 +0400

    add(versioning) query param versioning

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -160,4 +160,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_VERSIONING_CLASS':
+        'rest_framework.versioning.QueryParameterVersioning'
 }
